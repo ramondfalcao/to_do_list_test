2420f46b6c2b08df7808fbdfb972be825ef4917d
ramond_falcao <ramondfalcao@gmail.com>
2025-05-25 15:45:43 -0300

    feat: create user route

--- app/backend/src/users/dto/create-user.dto.ts
+++ app/backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+export class CreateUserDto {
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty({ required: false })
+  email: string;
+
+  @ApiProperty()
+  password: string;
+}
