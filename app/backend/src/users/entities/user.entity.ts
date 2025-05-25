fe9dbb0e1dac3f2ed27ed77778c4d1a70f49ccdb
ramond_falcao <ramondfalcao@gmail.com>
2025-05-25 15:57:43 -0300

    feat: create route list all users

--- app/backend/src/users/entities/user.entity.ts
+++ app/backend/src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+import { User } from 'generated/prisma';
+
+export class UserEntity implements User {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updateAt: Date;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  email: string;
+
+  @Exclude()
+  password: string;
+}
