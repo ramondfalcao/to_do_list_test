a42fffcdd25c37357a2537deb8d0813ff6af64ad
ramond_falcao <ramondfalcao@gmail.com>
2025-05-25 17:35:33 -0300

    feat: class validator in create user dto

--- app/backend/src/users/dto/create-user.dto.ts
+++ app/backend/src/users/dto/create-user.dto.ts
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   username: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   password: string;
 }
