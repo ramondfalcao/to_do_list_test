a60a8ec85e3d7b3ee95d69b305286b28923edc71
ramond_falcao <ramondfalcao@gmail.com>
2025-05-26 10:05:42 -0300

    Update Create Task DTO

--- app/backend/src/tasks/dto/create-task.dto.ts
+++ app/backend/src/tasks/dto/create-task.dto.ts
@@ -2,12 +2,9 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateTaskDto {
-    @IsNotEmpty()
-    @IsNotEmpty()
     @ApiProperty()
     id_user: number;
 
-    @IsNotEmpty()
     @IsNotEmpty()
     @ApiProperty()
     description: string;
